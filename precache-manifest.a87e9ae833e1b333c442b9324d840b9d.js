7edb85d2d352926ccc90f2df50f765ebfc935309
adirpion <adirpeer@gmail.com>
2019-12-30 21:11:24 +0200

    Updates

---
+++ precache-manifest.a87e9ae833e1b333c442b9324d840b9d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aad8758772cd26c35d814296c585b2fc",
+    "url": "/shop/index.html"
+  },
+  {
+    "revision": "e559aabca17c985d0017",
+    "url": "/shop/static/css/main.9f83e7ec.chunk.css"
+  },
+  {
+    "revision": "81f8b306cef4bac29e59",
+    "url": "/shop/static/js/2.d65b2015.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/shop/static/js/2.d65b2015.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e559aabca17c985d0017",
+    "url": "/shop/static/js/main.5916f986.chunk.js"
+  },
+  {
+    "revision": "b05d80a335d6b2056eb5",
+    "url": "/shop/static/js/runtime-main.640d9523.js"
+  }
+]);
